68b95bf098237955db6ae5d9098576c148abad6f
tomyChr <thomas.christen@ichristen.ch>
2022-05-05 07:19:25 +0200

    Changes for master detail feature

--- src/test/java/io/confluent/connect/jdbc/source/JdbcMasterDetailSourceTaskTest.java
+++ src/test/java/io/confluent/connect/jdbc/source/JdbcMasterDetailSourceTaskTest.java
@@ -14,7 +14,9 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import static org.easymock.EasyMock.*;
+import static org.easymock.EasyMock.anyObject;
+import static org.easymock.EasyMock.createMock;
+import static org.easymock.EasyMock.replay;
 import static org.junit.Assert.*;
 
 // @RunWith(PowerMockRunner.class)
